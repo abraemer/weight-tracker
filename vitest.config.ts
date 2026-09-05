b770fd1d47025942c55e0229b99b2e5c404d7913
Adrian Braemer <bradrian@gmail.com>
2026-09-06 00:56:55 +0200

    chore(deps): drop unused vite-plugin-eslint, eslint-plugin-prettier, @vue/test-utils; simplify vitest config

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vitest/config'
 import vue from '@vitejs/plugin-vue'
-import vuetify from 'vite-plugin-vuetify'
 import { fileURLToPath, URL } from 'node:url'
 
 export default defineConfig({
-  plugins: [vue(), vuetify({ autoImport: true })],
+  plugins: [vue()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src/frontend', import.meta.url)),
@@ -14,10 +13,5 @@ export default defineConfig({
     globals: true,
     environment: 'jsdom',
     include: ['tests/**/*.test.ts'],
-    server: {
-      deps: {
-        inline: ['vuetify'],
-      },
-    },
   },
 })
